52b67450617031dc3faeb2e14575ae9d2c722a6e
LSS-580812 <lss.cityhunter@gmail.com>
2023-08-18 04:55:20 +0800

    加入專案檔案。

---
+++ WesleyBaseEx/Controllers/WesleyController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WesleyBaseEx.Models;
+
+namespace WesleyBaseEx.Controllers
+{
+    public class WesleyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        /* ASP.NET Core MVC 使用模型繫結建立資料模型的編輯表單與基架產生器的操作
+         * https://www.youtube.com/watch?v=azAyvWveRhA&list=PLzs78Vw3QIyB9VkqihaB6XkcDbg8Zd5G5&index=23&ab_channel=%E8%A1%9B%E6%96%AF%E7%90%86%E9%A0%BB%E9%81%93%28wesley09171%29
+         * 
+        */
+        public IActionResult Ex023()
+        {
+            //https://localhost:7162/Wesley/Ex023
+            return View();
+        }
+
+        [HttpPost] //只允許 HTTP 的 POST 方法動作
+        [ValidateAntiForgeryToken] //用於防範偽造要求 CSRF 的屬性
+        public IActionResult Ex023(MessageModel_Cs obj_Val)
+        {
+            if (ModelState.IsValid)
+            { /*表單資料驗證通過
+               * 一般來說我們都會用 ModelState.IsValid 來檢查在「模型繫結」的過程中所做的 輸入驗證 (Input Validation) 與 模型驗證 (Model Validation) 是否成功。
+               * 注意：模型內資料全部要成功，若模型內表單無用到時，亦會發生錯誤情形(注意)
+               */
+                TempData["Title"] = obj_Val.Title_P;
+                TempData["Content"] = obj_Val.Content_P;
+                TempData["Email"] = obj_Val.Email_P;
+                return RedirectToAction("DisplayFormDate"); //將網頁移轉到 DisplayFormDate 位置
+                //return View("DisplayFormDate");
+            }
+            //return Content("驗證失敗=" + ModelState.IsValid.ToString());
+            return View(); //否則驗證失敗時-> 再回到檢視輸入頁面 Ex023.cshtml           
+        }
+
+
+        public IActionResult DisplayFormDate()
+        {
+            if(TempData.Count == 0)
+            { //當 TempData 字典物件鍵值為0(即無資料)
+                return Content("無任何資料內容");
+            }
+            return View();
+        }
+    }
+}
---
+++ WesleyBaseEx/Models/MessageModel_Cs.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WesleyBaseEx.Models
+{
+    //宣告公開類別(訊息資料模型)
+    public class MessageModel_Cs
+    {
+        [DisplayName("標題")]
+        //[StringLength(30, MinimumLength =4)] //驗證失敗-> The field 標題 must be a string with a minimum length of 4 and a maximum length of 30.
+        [Required(ErrorMessage = "請輸入登入帳號")] //必填資料-> 驗證失敗時-> 請輸入登入帳號
+        //[MinLength(6, ErrorMessage = "{0}不得低於{1}個字元")]
+        //[RegularExpression("[@]+", ErrorMessage = "{0}必須包含@字元")]
+        public string? Title_P { get; set; } //宣告公開字串屬性(標題)
+
+        [DisplayName("內容")]
+        public string? Content_P { get; set; } //宣告公開字串屬性(內容)
+
+        [Display(Name ="顧客e-Mail")] //欄位別名
+        [DataType(DataType.EmailAddress)] //電子郵件格式
+        [Required] //必填資料
+        [StringLength(50, MinimumLength =10)] //輸入字元最少要10字元，最大可輸入50字元
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
+        //[EmailAddress(ErrorMessage = "Invalid Email Address")]
+        //[RegularExpression(@"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2.}$")] //資料正則化，必須為列舉中字元
+        public string? Email_P { get; set; } //套用以上規則
+
+        /*
+        [DisplayName("顧客意見")]
+        [Required]
+        [StringLength(255)]
+        public string? Message_P { get; set; }
+        */
+    }
+}
